467ab62a13aa00e576487272383f65f3a8040448
trisua <tri@swmff.org>
2025-03-17 01:39:35 -0400

    add: do not remake database every dump

--- crates/oysters_core/src/persistance.rs
+++ crates/oysters_core/src/persistance.rs
@@ -14,22 +14,18 @@ where
 {
     /// Dump the cache into the dump file ([`PATH`]).
     pub fn dump(&self) -> Result<()> {
-        // replace database file
-        if fs::exists(PATH.as_ref()).unwrap_or(false) {
-            if let Err(e) = fs::remove_file(PATH.as_ref()) {
+        // create database file
+        if !fs::exists(PATH.as_ref()).unwrap_or(false) {
+            if let Err(e) = fs::write(PATH.to_string(), []) {
                 panic!("{}", e);
             };
         }
-
-        if let Err(e) = fs::write(PATH.to_string(), []) {
-            panic!("{}", e);
-        };
 
         // create database
         let conn = Connection::open(PATH.as_ref())?;
 
         conn.execute(
-            "CREATE TABLE map (
+            "CREATE TABLE IF NOT EXISTS map (
                 key   TEXT NOT NULL,
                 value TEXT NOT NULL,
                 used  INT
@@ -38,10 +34,39 @@ where
         )?;
 
         for (k, v) in &self.0 {
-            conn.execute(
-                "INSERT INTO \"map\" VALUES (?, ?, ?)",
-                (k.to_string(), v.0.to_string(), v.1.used),
-            )?;
+            // check if key exists
+            let mut query =
+                conn.prepare("SELECT \"key\", \"value\" FROM \"map\" WHERE \"key\" = ? LIMIT 1")?;
+            let selected = query.query([k.to_string()]).expect("failed to select");
+
+            let mut vec = Vec::new();
+            for row in selected.mapped(|row| {
+                Ok((
+                    row.get::<usize, String>(0)?,
+                    row.get::<usize, String>(1)?,
+                    row.get::<usize, usize>(2)?,
+                ))
+            }) {
+                vec.push(row.unwrap());
+            }
+
+            if vec.len() == 0 {
+                // doesn't exist yet
+                conn.execute(
+                    "INSERT INTO \"map\" VALUES (?, ?, ?)",
+                    (k.to_string(), v.0.to_string(), v.1.used),
+                )?;
+            } else {
+                // exists; update only if changed
+                let existing = vec.get(0).unwrap();
+
+                if (existing.1 != v.0.to_string()) | (existing.2 != v.1.used) {
+                    conn.execute(
+                        "UPDATE \"map\" SET \"value\" = ?, \"used\" = ? WHERE \"key\" = ?",
+                        (v.0.to_string(), v.1.used, k.to_string()),
+                    )?;
+                }
+            }
         }
 
         // return
