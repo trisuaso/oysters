2abc0b3db160945f3a8807dc438c13b4eac6321b
trisua <tri@swmff.org>
2025-03-16 23:41:08 -0400

    add: full systemd script

--- systemd/systemd_install.sh
+++ systemd/systemd_install.sh
@@ -1,6 +1,11 @@
 cargo install oysters oysters_cli
 sudo mkdir /etc/oysters
 
+cd /etc/oysters
+sudo wget https://raw.githubusercontent.com/trisuaso/oysters/refs/heads/master/.config/config.example.toml
+sudo mkdir .config
+sudo mv config.example.toml .config/config.toml
+
 cd /etc/systemd/system
 sudo wget https://raw.githubusercontent.com/trisuaso/oysters/refs/heads/master/systemd/oysters.service
 sudo wget https://raw.githubusercontent.com/trisuaso/oysters/refs/heads/master/systemd/oysters-dump.service
@@ -8,6 +13,16 @@ sudo wget https://raw.githubusercontent.com/trisuaso/oysters/refs/heads/master/s
 sudo wget https://raw.githubusercontent.com/trisuaso/oysters/refs/heads/master/systemd/oysters-scan.service
 sudo wget https://raw.githubusercontent.com/trisuaso/oysters/refs/heads/master/systemd/oysters-scan.timer
 
+sudo ln -s ~/.cargo/bin/oysters /usr/bin/oysters
+sudo ln -s ~/.cargo/bin/oysters-cli /usr/bin/oysters-cli
+
 sudo systemctl daemon-reload
+
 sudo systemctl enable --now oysters
 echo "Oysters service enabled"
+
+sudo systemctl enable oysters-scan.timer
+echo "Oysters scan timer enabled"
+
+sudo systemctl enable oysters-dump.timer
+echo "Oysters dump timer enabled"
