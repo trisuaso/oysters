28ebb6b972d22258e627fe29307b103aeeccac90
trisua <tri@swmff.org>
2025-03-19 21:28:13 -0400

    add(pagemap): ability to remove add(pagemap): replace instead of insert (keep correct page size)

--- crates/oysters_core/examples/pagemap.rs
+++ crates/oysters_core/examples/pagemap.rs
@@ -4,8 +4,8 @@ use oysters_core::*;
 fn main() {
     let mut book: pagemap::PageMap<String, String> =
         pagemap::PageMap::new(pagemap::PageMapOptions {
-            pages: 1,
-            page_size: 8_000,
+            pages: 4,
+            page_size: 24,
         });
 
     book.pagebook.insert(b"test", b"Hello, world!");
@@ -15,4 +15,12 @@ fn main() {
     book.pagebook.insert(b"test1", b"Hello, world! 1");
     let v = book.pagebook.get(b"test1");
     dbg!(&String::from_utf8(v.unwrap()));
+
+    book.pagebook.remove(b"test");
+    let v = book.pagebook.get(b"test");
+    dbg!(v.is_some());
+
+    book.pagebook.insert(b"test2", b"Hello, world! 2");
+    let v = book.pagebook.get_full(b"test2").unwrap();
+    dbg!(v.0, &String::from_utf8(v.1));
 }
--- crates/oysters_core/src/pagemap/pagebook.rs
+++ crates/oysters_core/src/pagemap/pagebook.rs
@@ -1,15 +1,33 @@
-pub struct PageBook(Vec<Vec<u8>>, pub (usize, usize));
+use std::ops::Range;
+
+pub struct PageBook(pub Vec<Vec<u8>>, pub (usize, usize));
 
 impl PageBook {
     /// Create a new [`PageBook`].
+    ///
+    /// The given `count` is just the _initial_ number of pages in the pagebook.
+    /// The book will grow if it runs out of room.
     pub fn new(count: usize, size: usize) -> Self {
         let mut pagebook = Vec::with_capacity(count);
 
         for _ in 0..count {
-            pagebook.push(Vec::with_capacity(size));
+            pagebook.push(vec![0; size]);
         }
 
         Self(pagebook, (count, size))
+    }
+
+    /// Get the next free (\0) range of the given size in the given page.
+    pub fn find_free_range(page: &[u8], size: usize) -> Option<Range<usize>> {
+        let windows = page.windows(size);
+
+        for (i, window) in windows.enumerate() {
+            if window == vec![0; size] {
+                return Some(i..i + size);
+            }
+        }
+
+        None
     }
 
     /// Find the page the given `key` blongs to.
@@ -29,8 +47,8 @@ impl PageBook {
             while let Some(chunk) = windows.next() {
                 let mut chunk: Vec<u8> = chunk.to_vec();
 
-                if chunk.get(0).unwrap() == &0_u8 {
-                    // chunks _might_ start with \0 because of being RIGHT after
+                if chunk.get(0).unwrap() == &2_u8 {
+                    // chunks _might_ start with \2 because of being RIGHT after
                     // a value chunk... we should just go ahead and remove it and ignore
                     chunk.remove(0);
                 }
@@ -60,6 +78,63 @@ impl PageBook {
         )
     }
 
+    /// Get a value from the pagebook (assuming we **don't** know the page number).
+    ///
+    /// Returns the page number as part of the output.
+    ///
+    /// # Arguments
+    /// * `key` - the key as bytes
+    pub fn get_full(&self, key: &[u8]) -> Option<(usize, Vec<u8>)> {
+        let page = match self.find_page(&key) {
+            Some((page, _)) => page,
+            None => return None,
+        };
+
+        if let Some(v) = self.get_known(page, key) {
+            Some((page, v))
+        } else {
+            None
+        }
+    }
+
+    /// Get the range of a key in the pagebook. Range includes from the start of
+    /// the key, to the end of the value (with terminating byte).
+    ///
+    /// # Arguments
+    /// * `page` - the page the key exists in
+    /// * `key` - the key as bytes
+    pub fn get_key_range(&self, page: usize, key: &[u8]) -> Option<Range<usize>> {
+        // get page
+        let page = match self.0.get(page) {
+            Some(p) => p,
+            None => return None,
+        };
+
+        // get value
+        let mut windows = page.windows(key.len());
+        let pos = match windows.position(|x| x == key) {
+            Some(p) => p,
+            None => return None,
+        };
+
+        let key_end = pos + key.len() + 1;
+
+        // collect bytes to terminating byte
+        let mut count: usize = 0;
+        let sub: &[u8] = &page[key_end..page.len()];
+
+        for byte in sub {
+            count += 1;
+
+            if byte == &2_u8 {
+                break;
+            }
+        }
+
+        // return
+        Some(pos..count)
+    }
+
     /// Get a value from the pagebook (assuming we know the page number).
     ///
     /// # Arguments
@@ -85,12 +160,12 @@ impl PageBook {
         let key_end = pos + key.len() + 1;
 
         // we can get the actual data by getting every byte UNTIL a null byte is found
-        // (\0 terminates values, \1 terminates keys)
+        // (\2 terminates values, \1 terminates keys)
         let mut out: Vec<u8> = Vec::new();
         let sub: &[u8] = &page[key_end..page.len()];
 
         for byte in sub {
-            if byte == &0_u8 {
+            if byte == &2_u8 {
                 break;
             }
 
@@ -107,34 +182,90 @@ impl PageBook {
     /// * `value` - the value as bytes
     pub fn insert(&mut self, key: &[u8], value: &[u8]) {
         // find good page
-        let page: usize = {
+        let page: (usize, Option<Range<usize>>) = {
             let mut num: usize = 0;
+            let mut range: Option<Range<usize>> = None;
             for (i, page) in self.0.iter().enumerate() {
-                if page.len() == self.1.1 {
+                let free_range = PageBook::find_free_range(page, key.len() + value.len() + 2);
+
+                if free_range.is_none() {
                     // page is full
                     continue;
                 }
 
                 num = i;
+                range = free_range;
             }
 
-            num
+            (num, range)
         };
 
         // select page as mutable
-        let page = self.0.get_mut(page).unwrap();
+        let free_range = page.1.unwrap();
+        let page = self.0.get_mut(page.0).unwrap();
 
         // push data
+        let mut out: Vec<u8> = Vec::new();
         for byte in key {
-            page.push(byte.to_owned());
+            out.push(byte.to_owned());
         }
 
-        page.push(1_u8);
+        out.push(1_u8);
 
         for byte in value {
-            page.push(byte.to_owned());
+            out.push(byte.to_owned());
         }
 
-        page.push(0_u8);
+        out.push(2_u8);
+
+        // swap data
+        let mut idx: usize = 0;
+        for i in free_range {
+            page.insert(i, out.get(idx).unwrap().to_owned());
+            idx += 1;
+        }
+    }
+
+    /// Remove a value from the pagebook (assuming we **don't** know the page number).
+    ///
+    /// # Arguments
+    /// * `key` - the key as bytes
+    pub fn remove(&mut self, key: &[u8]) -> Option<()> {
+        self.remove_known(
+            match self.find_page(&key) {
+                Some((page, _)) => page,
+                None => return None,
+            },
+            key,
+        )
+    }
+
+    /// Remove a key (and its value) from the given page.
+    ///
+    /// # Arguments
+    /// * `page` - the page the key exists in
+    /// * `key` - the key as bytes
+    pub fn remove_known(&mut self, page: usize, key: &[u8]) -> Option<()> {
+        // get range
+        let range = match self.get_key_range(page, key) {
+            Some(r) => r,
+            None => return None,
+        };
+
+        // get mut page
+        let page = match self.0.get_mut(page) {
+            Some(p) => p,
+            None => return None,
+        };
+
+        // remove
+        let mut removed_bytes: usize = 0;
+        for i in range {
+            page.remove(i - removed_bytes); // subtract the number of bytes we've already removed to account for changing len
+            removed_bytes += 1;
+        }
+
+        // return
+        Some(())
     }
 }
